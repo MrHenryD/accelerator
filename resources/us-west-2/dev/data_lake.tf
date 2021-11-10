cdbb28aab4aef4796253941eb93e4d89ebf9b951
henrydang26 <henrydang26@gmail.com>
2021-11-10 00:25:31 -0500

    add us-west-2 deployment

---
+++ resources/us-west-2/dev/data_lake.tf
@@ -0,0 +1,9 @@
+module "data_lake" {
+  source = "../../../modules/s3"
+
+  environment   = var.environment
+  bucket_name   = "xbnef-jklourp-data-lake-dev"
+  versioning    = true
+  mfa_delete    = true
+  
+}
